448751a13361b3d1c935334983066ca18d15a4ac
mark <mark.klibanov@myant.ca>
2017-09-12 20:43:27 -0400

    added signup functionality added salted hash

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json, jsonify
 from pymongo import MongoClient
+from werkzeug.security import generate_password_hash, check_password_hash
 
-client = MongoClient()
+
 app = Flask(__name__)
 
 
 #MongoDb Configuration
+client = MongoClient()
 db=client.Bucketlist
 accounts = db.accounts
 
@@ -17,14 +19,43 @@ def store_user(user_name, user_username, user_password):
             "password":user_password}
 
        post_id = accounts.insert_one(user).inserted_id
-       return post_id
+       return 0
    else:
-       print("User exists!")
+       return "User exists!"
 
 
-@app.route("/")
+@app.route('/')
 def main():
         return render_template('index.html')
+
+
+@app.route('/showSignUp')
+def showSignUp():
+        return render_template('signup.html')
+
+
+@app.route('/signUp', methods=['POST'])
+def signup():
+    _name=request.form['inputName']
+    _email=request.form['inputEmail']
+    _password=request.form['inputPassword']
+
+    if _name and _email and _password:
+
+        #create the salted hash
+        _hashed_password = generate_password_hash(_password)
+
+        #store the user credentials
+        data = store_user(_name, _email, _hashed_password)
+
+        if data == 0:
+            return json.dumps({'message':'User created succesfully!'})
+        else:
+            return json.dumps({'error': str(data)})
+
+
+    else:
+        return json.dumps('Enter the required field')
 
 
 if __name__ == "__main__":
